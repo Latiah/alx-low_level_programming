ffedd34127adc31f497ac077c312c7bc207d4f30
latiah <kimtifah2@gmail.com>
2022-03-11 12:40:33 -0800

    reversing letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char t = 'z';
+for (t = 'z'; t >= 'a'; t--)
+{
+putchar (t);
+}
+putchar ('\n');
+return (0);
+}

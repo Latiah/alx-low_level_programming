19b010ef57f7ade7f0a37be84b43ccfc9ce9d8ad
latiah <kimtifah2@gmail.com>
2022-03-11 13:10:32 -0800

    without E and Q letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,14 @@
  */
 int main(void)
 {
-  char l = 'a';
-  for(l = 'a'; l <= 'z'; l++)
-    {
-      if (l != e && q){
-	putchar(l);
-      }
-      else
-sss	{
-     ontinue
+char l = 'a';
+for (l = 'a'; l <= 'z'; l++)
+{
+if (l != 'e' &&  l != 'q')
+{
+putchar (l);
 }
 }
-putchar('\n');
+putchar ('\n');
 return (0);
 }

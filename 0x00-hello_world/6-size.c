4d330563bea263e005e3b981a37b05725387f0a4
latiah <kimtifah2@gmail.com>
2022-03-14 00:53:16 -0700

    sizes of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
+{
 #include<stdio.h>
+
+/**
+ * main Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 int a;
@@ -12,4 +19,5 @@ printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
+}
 }

84de278b0b25a5c6c31d3e7230c020fb04c0d86b
latiah <kimtifah2@gmail.com>
2022-03-14 05:15:54 -0700

    fprintf for displaying

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main -ENTRY POINT
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char s[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+fprintf(stderr, "%s\n", s);
+return (1);
+}

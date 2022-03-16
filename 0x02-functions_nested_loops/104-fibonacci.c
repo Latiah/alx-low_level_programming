de9f49de6accc681fd40ca9e16f0a7e11288c46e
latiah <kimtifah2@gmail.com>
2022-03-16 08:05:56 -0700

    fibanac

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,15 +3,15 @@
  * main -entry of our program
  *Return: 0 success
  */
-long double main(void)
+int main(void)
 {
-long double a, b, c, i;
+float  a, b, c, i;
 a = 0;
 b = 1;
 c = 1;
 for (i = 1; i <= 98; i++)
 {
-printf("%LF, ", c);
+printf("%f, ", c);
 a = b;
 b = c;
 c = a + b;

559bc91f3c4419c4c3849a29020c214c1c5d56fd
latiah <kimtifah2@gmail.com>
2022-03-16 09:09:32 -0700

    98 fibonac

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-long double a, b, c, i;
+long int a, b, c, i;
 a = 0;
 b = 1;
 c = 1;
 for (i = 1; i <= 98; i++)
 {
 if (i != 98)
-printf("%Lf, ", c);
+printf("%ld, ", c);
 else
-printf("%Lf", c);
+printf("%ld", c);
 a = b;
 b = c;
 c = a + b;

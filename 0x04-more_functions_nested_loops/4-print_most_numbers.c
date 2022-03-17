64e22a8ce7e69e2fa669f0c23324712c6f551299
latiah <kimtifah2@gmail.com>
2022-03-17 06:54:51 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers -printing numbers
+ */
+void print_most_numbers(void)
+{
+char k;
+for (k = '0'; k <= '9')
+{
+if ((k == '2') || (k == '4'))
+{
+continue;
+_putchar (k);
+}
+}
+_putchar ('\n');
+}

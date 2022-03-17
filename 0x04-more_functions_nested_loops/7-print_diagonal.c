a7da46a03efbabe87a8c1fe14c0070e0e88b89f7
latiah <kimtifah2@gmail.com>
2022-03-17 09:54:09 -0700

    push

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,8 @@ void print_diagonal(int n)
 int i;
 for (i = 1; i <= n; i++)
 {
-_putchar ("\\\n");
+_putchar ('\\');
+_putchar ('\n');
 }
-_putchar ("\n");
+_putchar ('\n');
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ for (i = 1; i <= size; i++)
 {
 for (j = 1; j <= size; j++)
 {
-_putchar ("#");
+_putchar ('#');
 }
-_putchar ("\n");
+_putchar ('\n');
 }
-_putchar ("\n");
+_putchar ('\n');
 }

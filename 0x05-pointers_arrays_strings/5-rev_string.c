1b102f7e1ddc89650beb4a42305caaba30722c18
latiah <kimtifah2@gmail.c>
2022-03-21 06:54:07 -0700

    string reversing

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "main.h"
+#include<string.h>
+/**
+ * rev_string -reversing a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+printf("%s\n", strrev(s));
+}

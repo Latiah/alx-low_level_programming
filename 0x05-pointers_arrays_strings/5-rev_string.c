ce63467c09fe7fbbe133dbd25bd0134832f2c244
latiah <kimtifah2@gmail.c>
2022-03-21 08:59:16 -0700

    reversing strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,19 @@
  */
 void rev_string(char *s)
 {
-printf("%s\n", strrev(s));
+int l, i;
+char *begin_ptr, *end_ptr, ch;
+l = strlen(s);
+begin_ptr = s;
+end_ptr = s;
+for (i = 0; i < l - 1; i++)
+end_ptr++;
+for (i = 0; i < l / 2; i++)
+{
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
+begin_ptr++;
+end_ptr--;
+}
 }

aeac89b96f2d2e42471db7b7fc643ca384b6d5f8
latiah <kimtifah2@gmail.com>
2022-03-22 10:05:38 -0700

    reversing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,23 +7,14 @@
  */
 void print_rev(char *s)
 {
-int l;
-int i;
-char ch;
-char *begin_ptr;
-char *end_ptr;
-l = strlen(s);
-begin_ptr = s;
-end_ptr = s;
-for (i = 0; i < l - 1; i++)
-end_ptr++;
-for (i = 0; i < l / 2; i++)
+int i = 0;
+while (*(s + i))
+i++;
+i = i - 1;
+while (i >= 0)
 {
-ch = *end_ptr;
-*end_ptr = *begin_ptr;
-*begin_ptr = ch;
-begin_ptr++;
-end_ptr--;
-printf("%s\n", s);
+putchar(*(s + i));
+i--;
 }
+putchar('\n');
 }

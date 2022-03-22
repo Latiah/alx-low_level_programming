34b9eeb44e456e84612ddb8156f4704e1f7dc0fe
latiah <kimtifah2@gmail.com>
2022-03-22 06:36:10 -0700

    strings are copied

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,6 +7,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-*dest = *src;
+if (dest == NULL)
+return (NULL);
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
 return (dest);
 }

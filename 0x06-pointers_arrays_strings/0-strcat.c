7cf6baa1ab86ab6a0705646c2f9b6a0e36589a65
latiah <kimtifah2@gmail.com>
2022-03-24 04:08:22 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-while (*dest)
-dest++;
-while (*src)
+int c, d;
+c = 0;
+while (dest[c] != '\0')
 {
-*dest = *src;
-src++;
-dest++;
+c++;
 }
-*dest = '\0';
+d = 0;
+while (src[d] != '\0')
+{
+dest[c] = src[d];
+d++;
+c++;
+}
+dest[c] = '\0';
 return (dest);
 }
+

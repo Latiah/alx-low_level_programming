68aef2c3746b8af6026f9294bd51da7ce3a6efd9
latiah <kimtifah2@gmail.com>
2022-03-30 04:02:41 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts_recursion -a function that prints a string followed by anew line
+ * @s: string entered
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}

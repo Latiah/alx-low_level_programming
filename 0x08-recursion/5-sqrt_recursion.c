3f58f433f8d118fe2ced3d752d76b7d214798060
latiah <kimtifah2@gmail.com>
2022-03-30 13:07:50 -0700

    square root recursively

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,18 @@
 #include "main.h"
-#include<math.h>
+/**
+ * sqroot - find square root
+ * @num: number entered
+ * @res: result
+ * Return: what to return
+ */
+int sqroot(int num, int res)
+{
+if (num * num == res)
+return (num);
+if (num * num < 0)
+return (-1);
+return (sqroot(num + 1, res));
+}
 /**
  * _sqrt_recursion - returns square root of a number
  * @n: number
@@ -7,9 +20,9 @@
  */
 int _sqrt_recursion(int n)
 {
-int result;
+if (n == 0)
+return (0);
 if (n < 0)
 return (-1);
-result = sqrt(n);
-return (result);
+return (sqroot(1, n));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqroot(int num, int res);
 int is_prime_number(int n);
 int prime(int num, int p);
 int length(char *st);

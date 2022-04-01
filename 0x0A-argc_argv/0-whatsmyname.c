4013da71711c69ae493555dad28722e46fc57c9d
latiah <kimtifah2@gmail.com>
2022-04-01 03:23:50 -0700

    name of a program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main -start of a program
+ * @argc: argument count
+ * @argv:argument vector
+ * Return: 0 always a success
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

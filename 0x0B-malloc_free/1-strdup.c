8b56ead8c9b81b108e364bc55482537a829eb572
latiah <kimtifah2@gmail.c>
2022-04-04 10:02:55 -0700

    copying a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * _strdup - copies string
+ * @str: string
+ * Return: what is returned
+ */
+char *_strdup(const char *str)
+{
+char *dest = malloc(strlen(str) + 1);
+if (dest == NULL)
+return (NULL);
+strcpy(dest, str);
+return (dest);
+}

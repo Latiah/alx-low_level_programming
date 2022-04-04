94abd488e6aacb5bd38f584b031f4af12617600d
latiah <kimtifah2@gmail.c>
2022-04-04 15:41:33 -0700

    concate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,15 +9,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int i;
-int s1_length = strlen(s1);
-int s2_length = strlen(s2);
-int size = s1_length + s2_length + 1;
-char *s = malloc(sizeof(char) * size);
-for (i = 0; i < s1_length; i++)
-s[i] = s1[i];
-for (i = 0; i < s2_length; i++)
-s[s1_length + i] = s2[i];
-s[size - 1] = '\0';
+int i, j;
+int size, k;
+char *s;
+if (s1 == NULL)
+i = 0;
+else
+{
+for (i = 0; s1[i]; i++)
+;
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+size = i + j + 1;
+s = malloc(size *sizeof(char));
+if (s == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[i + j] = s2[k];
+s[i + j] = '\0';
 return (s);
 }

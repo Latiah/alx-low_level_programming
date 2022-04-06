9412a35c43dae6c7408910746c3b37d2b6ff4b5c
latiah <kimtifah2@gmail.c>
2022-04-06 06:45:19 -0700

    allocating memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,8 +5,9 @@
 /**
  * malloc_checked -a function that allocates memory using malloc
  * @b:size of memory needed
+ * Return: returned
  */
-void malloc_checked(unsigned int b)
+void *malloc_checked(unsigned int b)
 {
 void *ptr;
 ptr = malloc(b);

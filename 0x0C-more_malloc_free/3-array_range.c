8cce3eddf5d383007f7f52d21951450c35bea098
latiah <kimtifah2@gmail.c>
2022-04-06 08:12:16 -0700

    array created

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,9 +12,9 @@ int *array_range(int min, int max)
 int n;
 int i;
 int *p;
-n = max - min + 1
 if (min > max)
 return (NULL);
+n = max - min + 1;
 p = malloc(sizeof(int) * n);
 if (p == NULL)
 return (NULL);

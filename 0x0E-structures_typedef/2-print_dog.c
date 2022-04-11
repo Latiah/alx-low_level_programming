cfd8c8faf17349641799e6dfc69086bc75ccb036
latiah <kimtifah2@gmail.com>
2022-04-11 08:51:19 -0700

    printing elements of a structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include<string.h>
 #include<stdio.h>
 /**
  * print_dog - printing a structure
@@ -7,7 +8,19 @@
  */
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
+if (d->name != NULL)
 printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
+if (d->age != NULL)
 printf("Age: %f\n", d->age);
+else
+printf("Age: (nil)\n");
+if (d->owner != NULL)
 printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
 }

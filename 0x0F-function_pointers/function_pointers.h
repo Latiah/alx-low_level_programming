0db441407875669e59b9850a9f81c5c6565e381e
latiah <kimtifah2@gmail.com>
2022-04-13 04:04:50 -0700

    print

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifnedf FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 

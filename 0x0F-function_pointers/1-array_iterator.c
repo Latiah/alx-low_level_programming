5a6d4b7f318fc9269eb6151e1ad6c683bae946f7
latiah <kimtifah2@gmail.com>
2022-04-13 04:57:53 -0700

    array with function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include<stddef.h>
 #include<stdio.h>
 /**
  * array_iterator -function that deal with arrays
@@ -8,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
-int i;
+size_t i;
 for (i = 0; i < size; i++)
 {
 action(array[i]);

b6483f766f6f584d29c498e6837bfaee3df43ae3
latiah <kimtifah2@gmail.com>
2022-04-13 05:26:14 -0700

    printing name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include<stdio.h>
+#include<string.h>
 /**
  * print_name - afunction that print a name
  * @name: name entered
@@ -7,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
 f(name);
 }

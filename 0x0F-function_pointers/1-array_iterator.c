bb2b02908886caab659af05609060d8d93008ad8
latiah <kimtifah2@gmail.com>
2022-04-13 04:44:06 -0700

    array problem

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include<stdio.h>
+/**
+ * array_iterator -function that deal with arrays
+ * @array: array name
+ * @size: size of an array
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+int i;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}

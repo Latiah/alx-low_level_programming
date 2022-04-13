940f687bfe1da433c31425302f9c38c3d199b720
latiah <kimtifah2@gmail.com>
2022-04-13 14:12:55 -0700

    calculator with operatorsi

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include<stdio.h>
+/**
+ * op_add -returns some of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: value returned
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - returns the differece between two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: value returned
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - returns the product of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: value returned
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: value returned
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - returns the remainder
+ * @a: first number
+ * @b: second number
+ * Return: value returned
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}

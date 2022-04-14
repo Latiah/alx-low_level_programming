9978053cc2b31f437b3f41a254df484b3fa66184
latiah <kimtifah2@gmail.com>
2022-04-14 04:15:46 -0700

    error corrected

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,10 +7,10 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+unsigned int i;
+int sum = 0;
 va_list ap;
 va_start(ap, n);
-int i = 0;
-int sum = 0;
 for (i = 0; i < n; i++)
 sum += va_arg(ap, unsigned int);
 return (sum);

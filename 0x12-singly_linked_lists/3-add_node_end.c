26eb2c884e842f80ee4b4a15b8b9cf1960168932
latiah <kimtifah2@gmail.com>
2022-04-21 08:46:19 -0700

    node at the end of the list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,12 +3,12 @@
 #include "lists.h"
 #include<string.h>
 /**
- * add_node - adds node at the beginning of a list
+ * add_node_end - adds node at the beginning of a list
  * @head: node at the beginning of a list
  * @str: string
  * Return: what is returned
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node;
 list_t *last = *head;

8f9d2ea750f5d155d6321139f0beb510bf4e5572
latiah <kimtifah2@gmail.com>
2022-04-25 15:09:10 -0700

    number of elemnts in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number of elements
+ * @h:structure object
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+unsigned int i;
+while (h)
+{
+h = h->next;
+i++;
+}
+return (i);
+}

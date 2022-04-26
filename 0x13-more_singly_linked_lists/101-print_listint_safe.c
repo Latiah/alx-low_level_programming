4c1c899943ae08c0f5a583287ffbe2856a93c264
Latifah <kimtifah2@gmail.com>
2022-04-26 23:47:55 +0200

    printing list in safe version

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include<string.h>
+/**
+ * free_listint_safe - function that frees a listint_t list
+ * @head: list
+ * Return: return listint
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}

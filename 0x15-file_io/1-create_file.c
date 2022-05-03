89b6020a82a3d256c00cb665e957fdabb21fa56c
latiah <kimtifah2@gmail.com>
2022-05-03 04:50:01 -0700

    creation of a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -17,7 +18,7 @@ ssize_t written;
 int fd, len;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O - RDWR | O - CREAT | O - TRUNC);
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
